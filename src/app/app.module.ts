acd815e87a1b9a98e41149dc20671dc077a8568d
DreamerKing <dreamingking@live.cn>
2018-04-24 12:30:34 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModule }  from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
---
+++ src/app/model.ts
@@ -0,0 +1,20 @@
+export class Model {
+    user;
+    items;
+
+    constructor() {
+        this.user = "Admoa";
+        this.items = [new TodoItem("Buy Flowers", false),
+                            new TodoItem("Get Shoes", false),
+                            new TodoItem("Collecet Ticket", false)]
+    }
+}
+
+export class TodoItem {
+    action;
+    done;
+    constructor(actions, done){
+        this.action = actions;
+        this.done = done;
+    }
+}
