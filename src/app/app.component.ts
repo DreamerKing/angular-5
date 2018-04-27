3f91d1fa270f01b1990bcb67243c80393cc3e66a
DreamerKing <dreamingking@live.cn>
2018-04-27 22:48:34 +0800

    add order cart rest datesource cart detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Model, TodoItem } from './model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: "<router-outlet></router-outlet>"
+   templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,43 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgbModule }  from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from "@angular/router";
 import { StoreModule } from "./store/store.module";
+import { StoreComponent } from "./store/store.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [ AppComponent ],
   imports: [
     BrowserModule,
     StoreModule,
+    RouterModule.forRoot([
+      {
+        path: "store",
+        component: StoreComponent,
+        canActivate: [ StoreFirstGuard ]
+      }, {
+        path: "cart",
+        component : CartDetailComponent,
+        canActivate : [StoreFirstGuard],
+      }, {
+        path: "checkout",
+        component: CheckoutComponent,
+       canActivate : [StoreFirstGuard],
+      }, {
+        path: "**",
+        redirectTo: "/store"
+      }
+    ]),
     FormsModule,
     NgbModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ StoreFirstGuard ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {
  }
